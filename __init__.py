9bc07f4899709bed34089508c30ddd996b044afd
Daniel Bälz <info@dbaelz.de>
2025-07-10 11:34:39 +0200

    Initial project generated by comfy command line tool

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for just_some_math."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """"""
+__email__ = "you@gmail.com"
+__version__ = "0.0.1"
+
+from .src.just_some_math.nodes import NODE_CLASS_MAPPINGS
+from .src.just_some_math.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
